f9bbdc4590a42e07b010dbd3dc300dc50ad4be33
Gardient <gardient@users.noreply.github.com>
2021-11-08 16:57:04 +0200

    update read util

--- utils/read.sh
+++ utils/read.sh
@@ -2,12 +2,13 @@ BASEDIR="$(dirname "${BASH_SOURCE[0]}")"
 source "$BASEDIR/print.sh"
 
 prompt_and_read() {
-    prmpt="$1"
-    deflt="$2"
-    out="$3"
+    local prmpt="$1"
+    local deflt="$2"
+    local out="$3"
 
     echo -n "$prmpt"
-    if [[ -n "$deflt" ]]; then echo "[$deflt]"; else echo '' fi
+    if [[ -n "$deflt" ]]; then echo " [$deflt]:"; else echo ':'; fi
+    echo -n '> '
     read inpt
     if [[ -z "$inpt" && -n "$deflt" ]]; then
         inpt="$deflt"
