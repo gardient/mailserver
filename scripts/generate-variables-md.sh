023289ba32d5880fe70ae0440a97e56f0916fece
Gardient <gardient@users.noreply.github.com>
2021-11-09 21:26:53 +0200

    updates variable.md generation

--- scripts/generate-variables-md.sh
+++ scripts/generate-variables-md.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+vars=$(egrep -rsho '\$MAILSRV_[[:alpha:]_]+' . | sort | uniq)
+
 cat <<- EOF
 # Variables
 
@@ -10,7 +12,7 @@ They will be read from the environment
 Don't forget to \`export\` them
 
 \`\`\`sh
-$(egrep -rsho '\$MAILSRV_[[:alpha:]_]+' . | sort | uniq | fgrep -v 'MAILSRV_SA_')
+$(fgrep -v -e 'MAILSRV_SA_' -e 'MAILSRV_INSTALL' <<< "$vars")
 \`\`\`
 
 If you want to have a truly headless instalation you will have to define them all
@@ -21,6 +23,14 @@ In addition you can use the following to not generate the DB passwords for the
 service accounts used
 
 \`\`\`sh
-$(egrep -rsho '\$MAILSRV_[[:alpha:]_]+' . | sort | uniq | fgrep 'MAILSRV_SA_')
+$(fgrep 'MAILSRV_SA_' <<< "$vars")
+\`\`\`
+
+# Installer overwrites
+
+the following overwrites the bootstrappers behaviour
+
+\`\`\`sh
+$(fgrep 'MAILSRV_INSTALL' <<< "$vars")
 \`\`\`
 EOF
