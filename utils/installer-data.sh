6a8e2427d9c5d81f84dd21c055ebb9795a56b977
Gyula Kerezsi <gardient@users.noreply.github.com>
2021-11-09 10:06:32 +0200

    added generation of passwords for DB accounts (#16)
    closes #3

--- utils/installer-data.sh
+++ utils/installer-data.sh
@@ -1,33 +1,37 @@
 BASEDIR="$(dirname "${BASH_SOURCE[0]}")"
 source "$BASEDIR/env.sh"
 
+_inst_data_getpath() {
+    echo "$INSTALLER_DATA/$1.dat"
+}
+
 _inst_data_load() {
-    local store="$1"
+    local store="$(_inst_data_getpath $1)"
     local key val
     set -a
-    if [[ -f "$INSTALLER_DATA/$store.dta" ]]; then
+    if [[ -f "$store" ]]; then
         while read -r line; do
             if [[ $line =~ ^[[:alpha:]_]*=.*$ ]]; then
                 key=$(cut -d= -f1 <<< "$line" | sed 's/[[:space:]]*$//')
                 val=$(cut -d= -f2- <<< "$line" | sed -e 's/^[[:space:]]*//' -e s/^\'// -e s/\'$//)
                 __ensure_env "$key" "$val"
             fi
-        done < "$INSTALLER_DATA/$store.dta"
+        done < "$store"
     fi
     set +a
 }
 
 _inst_data_save() {
-    local store="$1"
+    local store="$(_inst_data_getpath $1)"
     local key="$2"
     local val="$3"
 
-    if [[ -f "$INSTALLER_DATA/$store.dta" ]]; then
+    if [[ -f "$store" ]]; then
         # the lazy way of removing a key from the store
-        egrep -v "^$key[[:space:]]*=.*$" "$INSTALLER_DATA/$store.dta" > "$INSTALLER_DATA/$store.dta.swp"
-        mv "$INSTALLER_DATA/$store.dta.swp" "$INSTALLER_DATA/$store.dta"
+        egrep -v "^$key[[:space:]]*=.*$" "$store" > "$store.swp"
+        mv "$store.swp" "$store"
     fi
 
-    echo "$key='$val'" >> "$INSTALLER_DATA/$store.dta"
-    chmod 600 "$INSTALLER_DATA/$store.dta"
+    echo "$key='$val'" >> "$store"
+    chmod 600 "$store"
 }
