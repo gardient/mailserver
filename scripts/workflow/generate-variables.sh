685c9eeee894e896c0fd382c4a0a26b33c37501f
Gardient <gardient@users.noreply.github.com>
2021-11-10 15:09:46 +0200

    checkout before commiting in PRs

--- scripts/workflow/generate-variables.sh
+++ scripts/workflow/generate-variables.sh
@@ -6,6 +6,9 @@ if cmp -s "VARIABLES.md" "/tmp/VARIABLES.md"; then
     echo "VARIABLES.md unchanged"
 else
     echo "regenerated VARIABLES.md..."
+    if [[ -n "$PR_BRANCH" ]]; then
+        git checkout $PR_BRANCH
+    fi
     echo "commiting..."
     mv "/tmp/VARIABLES.md" "VARIABLES.md"
     git add "VARIABLES.md"
